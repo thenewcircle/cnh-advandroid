848ef85d8db6857fca058eea40baee17c90bd697
Marko Gargenta <marko@marakana.com>
2012-03-22 12:23:07 -0700

    Working proof of concept

--- UIApp/src/com/marakana/android/uiapp/UIAppActivity.java
+++ UIApp/src/com/marakana/android/uiapp/UIAppActivity.java
@@ -1,11 +1,11 @@
 package com.marakana.android.uiapp;
 
-import dalvik.system.DexClassLoader;
-
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.util.Log;
+import dalvik.system.DexClassLoader;
 
 public class UIAppActivity extends Activity {
 	static final String TAG = UIAppActivity.class.getSimpleName();
@@ -16,16 +16,19 @@ public class UIAppActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		DexClassLoader cl = new DexClassLoader("/sdcard/components.jar",
+		DexClassLoader cl = new DexClassLoader("/sdcard/UIComponents.apk",
 				"/sdcard/", null, getClass().getClassLoader());
- 
-		try { 
-			Class clazz = cl.loadClass("com.marakana.android.uicomponents.ComponentFragment");
-			Log.d(TAG, "GOT CLASS: " + clazz.toString());
+		Log.d(TAG, "resource: "+cl.getResource("res/string/app_name"));
 
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, (Fragment) clazz.newInstance())
-					.commit();
+		try {
+			Class clazz = cl
+					.loadClass("com.marakana.android.uicomponents.ComponentFragment");
+			Log.d(TAG, "GOT CLASS: " + clazz.toString());
+ 
+			FragmentTransaction t = getFragmentManager().beginTransaction();
+			t.setCustomAnimations(R.anim.slide, R.anim.slide);
+			t.add(R.id.container, (Fragment) clazz.newInstance());
+			t.commit();
 			Log.d(TAG, "DONE");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
